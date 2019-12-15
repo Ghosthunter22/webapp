17d3b3a2fb00d3f5f83e0e1600b95729a8ee2512
Ghosthunter22 <marcos-andreas@windowslive.com>
2019-12-15 15:59:32 +0200

    Users index pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::paginate(12);
+        $users = User::paginate(7);
         return view('users.index', ['users' => $users]);
     }
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,11 +6,14 @@
 <div class="modal-header">
     <h2>Users:</h2>
 </div>
-<ul>
+<ul class="pagination" style="width:0px">
+    <div class="container" style="margin-top:40px;">
     @foreach ($users as $user)
-        <div class="card" style="margin-top: 10px; width:250px">
+        <li class="card" style="margin-top: 10px; margin-left:50px; width:250px">
             <a href="{{ route('users.show', ['id' => $user->id]) }}" class="card-header"><img class="rounded-circle" src="/storage/avatars/{{ $user->avatar }}" width="40" height="40"/>{{ $user->name }}</a>
-        </div>
+        </li>
     @endforeach
+    </div>
+    {{ $users->links() }}
 </ul>
 @endsection
